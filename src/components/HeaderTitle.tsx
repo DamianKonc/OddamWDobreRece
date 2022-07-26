551d54dcb11bc15e2b820f4231ad295afbb9fe29
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 <damian.koncewicz@wp.pl>
2022-07-26 16:53:08 +0200

    header title

---
+++ src/components/HeaderTitle.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const HeaderTitle = () => {
+  return (
+    <div className="headerTitle">
+      <h1 className="headerTitle__title">
+        <p>Zacznij pomagać!</p>
+        <p>Oddaj niechciane rzeczy w zaufane ręce</p>
+      </h1>
+      <div className="headerTitle__img" />
+    </div>
+  );
+};
+
+export default HeaderTitle;
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import Navigation from "./Navigation";
+import HeaderTitle from "./HeaderTitle";
 
 const Hero = () => {
   return (
-    <div>
-      <div>
-        <Navigation />
-      </div>
+    <div className="hero">
+      <Navigation />
+      <HeaderTitle />
     </div>
   );
 };
