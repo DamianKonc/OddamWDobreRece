dc204cf52f4cc047d0a7c7f29793f9ffb1b560c8
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 <damian.koncewicz@wp.pl>
2022-07-28 21:45:27 +0200

    config firebase

---
+++ src/app/firebaceConfig.tsx
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAgOt-ttMMatnMTjDr4OMkBzO1gQ_Bw25o",
+  authDomain: "oddajrzeczy-3cd5a.firebaseapp.com",
+  projectId: "oddajrzeczy-3cd5a",
+  storageBucket: "oddajrzeczy-3cd5a.appspot.com",
+  messagingSenderId: "900446550258",
+  appId: "1:900446550258:web:c2b9da4559c242fe04786b",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
